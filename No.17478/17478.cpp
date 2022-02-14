0005866299d2f2cd4570a96c0523e4de2ba87f9b
mongsiri <cellism@naver.com>
2022-02-14 09:29:00 +0900

    initial commit

---
+++ No.17478/17478.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int recusiveFunc()
+{
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N = 0;
+
+    cout<< "N :";
+    cin>>N;
+    cout<<"Number is "<<N ;
+
+    return 0;
+}
