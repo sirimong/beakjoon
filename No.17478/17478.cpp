0c33cc702edda60f6de5408cce10a23d802a0675
mongsiri <cellism@naver.com>
2022-02-14 09:52:20 +0900

    📝

--- No.17478/17478.cpp
+++ No.17478/17478.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int recusiveFunc()
-{
-}
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
     int N = 0;
 
     cout<< "N :";
